f888d64d144ffb80522c5a3ab42c7752fa3f4eb8
Burak <brkdmn1903@icloud.com>
2022-04-22 01:04:20 +0300

    Update HesapMakinesi.java

--- Hesap Makinesi/HesapMakinesi.java
+++ Hesap Makinesi/HesapMakinesi.java
@@ -5,35 +5,35 @@ public class HesapMakinesi {
 		int n1,n2,selected;
 		Scanner input = new Scanner(System.in);
 		
-		System.out.print("İlk Sayýyý Giriniz: ");
+		System.out.print("İlk Sayıyı Giriniz: ");
 		n1 = input.nextInt();
-		System.out.print("Ýkinci Sayýyý Giriniz: ");
+		System.out.print("İkinci Sayıyı Giriniz: ");
 		n2 = input.nextInt();
 		
-		System.out.println("\n1-Toplama\n2-Çýkarma\n3-Çarpma\n4-Bölme\n");
+		System.out.println("\n1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme\n");
 		System.out.print("Seçiminiz: ");
 		selected = input.nextInt();
 		
 		switch (selected) {
 		case 1:
-			System.out.println("Toplamý: "+ (n1+n2));
+			System.out.println("Toplamı: "+ (n1+n2));
 			break;
 		case 2:
-			System.out.println("Farký: "+ (n1-n2));
+			System.out.println("Farkı: "+ (n1-n2));
 			break;
 		case 3:
-			System.out.println("Çarpýmý: "+ (n1*n2));
+			System.out.println("Çarpımı: "+ (n1*n2));
 			break;
 		case 4:
 			try {
 				System.out.println("Bölümü: "+ (n1/n2));
 				break;
 			} catch (Exception e) {
-				System.out.println("Bir sayý 0'a bölünemez!");
+				System.out.println("Bir sayı 0'a bölünemez!");
 				break;
 			}
 		default:
-			System.out.println("Geçersiz Giriþ!");
+			System.out.println("Geçersiz Giriş!");
 			break;
 		}
 		input.close();
