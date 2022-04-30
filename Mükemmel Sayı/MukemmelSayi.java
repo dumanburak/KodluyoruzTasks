c6c2cbcd12bc139575e21be77f6fb8b02c36906b
Burak <brkdmn1903@icloud.com>
2022-05-01 00:45:07 +0300

    Add files via upload

---
+++ Mükemmel Sayı/MukemmelSayi.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class MukemmelSayi {
+
+	public static void main(String[] args) {
+		int sayi, toplam = 0;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Bir sayi giriniz: ");
+		sayi = input.nextInt();
+		
+		for (int i = 1; i < sayi; i++) {
+			if (sayi%i==0)
+				toplam+=i;
+		}
+		if (toplam==sayi)
+			System.out.println(sayi+" Mükemmel sayıdır");
+		else
+			System.out.println(sayi + " Mükemmel sayı değildir");
+		
+		input.close();
+	}
+}
