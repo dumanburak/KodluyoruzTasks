3a7862f14e1c3a7fa7143b53d3d5e9654f9cd170
Burak <brkdmn1903@icloud.com>
2022-05-01 13:47:26 +0300

    Add files via upload

---
+++ Elmas/Elmas.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Elmas {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Bir Sayý Giriniz :");
+        int sayi = input.nextInt();
+        System.out.println();
+
+        for (int i = 0; i < sayi ; i++) {
+            for (int j = 1; j < (sayi - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        
+        for (int i = (sayi-2); i < sayi; i--) {
+        	if (i==-1)
+        		break;
+			for (int j = 1; j < (sayi - i); j++) {
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= (2 * i + 1); k++) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+        input.close();
+	}
+}
