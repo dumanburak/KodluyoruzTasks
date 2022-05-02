bbd707ee59d0524df0799f9f0b2473eda1159890
Burak <brkdmn1903@icloud.com>
2022-05-02 23:13:25 +0300

    Add files via upload

---
+++ Matrisin Transpozu/MatrisinTranspozu.java
@@ -0,0 +1,29 @@
+
+public class MatrisinTranspozu {
+
+	public static void yazdir(int [][] arr){
+        for (int [] satir: arr){
+            for (int sutun:satir){
+                System.out.print(sutun + "  ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public static void main(String[] args) {
+        int [][] matris= {{2,3,4},{5,6,4}};
+        int [][] transpose=new int[3][2];
+        for (int i=0;i<2;i++){
+            for (int j=0;j<3;j++){
+                transpose[j][i]=matris[i][j];
+            }
+        }
+        System.out.println("Matris");
+        yazdir(matris);
+        System.out.println("Transpoze : ");
+        yazdir(transpose);
+
+
+    }
+
+}
